b4cd02ab56a398f490342cbcaccd399ea90d4a79
didmaehd <didmaehd@gmail.com>
2023-01-18 23:04:51 +0900

    20230118#01

--- Day4-1_Randomization.py
+++ Day4-1_Randomization.py
@@ -8,3 +8,8 @@ print(random_float*5)
 
 love_score = random.randint(1,100)
 print(f"Your love score is {love_score}.")
+
+
+list = ["Head","Tail"]
+saikoro = random.choice(list)
+print(saikoro)
---
+++ main.py
@@ -0,0 +1,8 @@
+#list 
+
+list = ["A","B","C"]
+
+list.append("D")
+list.extend(["E","F"])
+
+print(list)

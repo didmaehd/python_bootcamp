97a08475c9ec1bd0f42a7dfecfb62582ed190f9c
didmaehd <didmaehd@gmail.com>
2023-02-16 18:02:33 +0900

    20220216#02

--- Day14#04_Higher-Lower.py
+++ Day14#04_Higher-Lower.py
@@ -12,19 +12,27 @@ def format_data(account):
     return f"{account_name}, a {account_descr}, from {account_country}"
 
 def chcek_answer(guess,a_followers,b_followers):
-    if a_followers > b_followers:
-        return guess == "a"
-    else:
-        return guess == "b"
-
+    if guess == "a":
+        if a_followers > b_followers:
+            return "success_a"
+        else :
+            return False
+    if guess == "b":
+        if a_followers > b_followers:
+            return False
+        else :
+            return "success_b"
+    # if a_followers > b_followers:
+    #     return guess == "guess"
+    # else:
+    #     return guess == "b"
 
 print(logo)
 score = 0
-account_b = random.choice(data)
+account_a = random.choice(data)
 
 game_should_continue = True
 while game_should_continue :
-    account_a = account_b
     account_b = random.choice(data)
 
     while account_a == account_b:
@@ -32,8 +40,7 @@ while game_should_continue :
 
     print(f"Compare A : {format_data(account_a)}")
     print(vs)
-    print(f"Against B : {format_data(account_b)}")    
-
+    print(f"Against B : {format_data(account_b)}")
     a_followers_count = account_a["follower_count"]
     b_followers_count = account_b["follower_count"]
 
@@ -43,10 +50,18 @@ while game_should_continue :
     os.system("cls")
     is_correct = chcek_answer(guess,a_followers_count,b_followers_count)
 
-    if is_correct :
+    if is_correct == "success_a":
         score += 1
+        print (f"You're right! Current score : {score}")
+    elif is_correct == "success_b":
+        score += 1
+        account_a = account_b
         print (f"You're right! Current score : {score}")
     else :
         game_should_continue = False
         print(f"Sorry, that's wrong. final score : {score}")
-    
+
+
+#이러면 어떤 경우든 B값이 A값으로 치환되는데.
+#A가 정답이면 A가 그대로이고
+#B가 정답이면 B가 A가 되어야함
--- main.py
+++ main.py
@@ -1,60 +1,64 @@
-# #딕셔너리에서 밸류값이 가장 큰 키 찾기
-# dict = {"a":4, "b":6,"c":9,"d":3,"e":7}
-# score = 0
-# winner = "o"
-# for name in dict:
-#     if dict[name] > score :
-#         score = dict[name]
-#         winner =  name
-#         print(name)
-# print(winner, score)
+# # #딕셔너리에서 밸류값이 가장 큰 키 찾기
+# # dict = {"a":4, "b":6,"c":9,"d":3,"e":7}
+# # score = 0
+# # winner = "o"
+# # for name in dict:
+# #     if dict[name] > score :
+# #         score = dict[name]
+# #         winner =  name
+# #         print(name)
+# # print(winner, score)
 
 
 
-# dict = {"a":"A", "b":6,"c":9,"d":3,"e":7}
-# print (dict["a"])
+# dict={"a":"A","b":6,"c":9,"d":3,"e":7}
+# # print (dict["a"])
+
+# # import random
+# # result = random.randrange(1,11)
+# # print(result)
+
+# # a = "global"
+# # def tykim():
+# #     a = "local"
+# #     print(a)
+
+# # tykim()
+# # print(a)
+
+
+# #함수 개념
+# #인풋을 받아서 어떤 처리를 거친후에 아웃풋을 만들어야 하는 경우
+# #나온 결과를 재활용 할 경우
+
+# #인풋을 받아서 
+
+# # def testa (a):
+# #     return a + 100
+
+
+# # def testb (b):
+# #     return testa(100) + b
+
+# # result = testb(100)
+# # print(result) 
 
 # import random
-# result = random.randrange(1,11)
-# print(result)
+# dic = [
+#     {
+#     "이름":"진", 
+#     "팔로어": 234,
+#     },
+#     {
+#     "이름":"뷔", 
+#     "팔로어 ": 342,
+#     }
+#        ]
 
-# a = "global"
-# def tykim():
-#     a = "local"
-#     print(a)
-
-# tykim()
-# print(a)
-
-
-#함수 개념
-#인풋을 받아서 어떤 처리를 거친후에 아웃풋을 만들어야 하는 경우
-#나온 결과를 재활용 할 경우
-
-#인풋을 받아서 
-
-# def testa (a):
-#     return a + 100
+# a = random.choice(dic)
+# print(a["이름"])
+# print(a["팔로어"])
 
 
-# def testb (b):
-#     return testa(100) + b
-
-# result = testb(100)
-# print(result) 
-
-import random
-dic = [
-    {
-    "이름":"진", 
-    "팔로어": 234,
-    },
-    {
-    "이름":"뷔", 
-    "팔로어 ": 342,
-    }
-       ]
-
-a = random.choice(dic)
-print(a["이름"])
-print(a["팔로어"])
+dic = {"asa": 1, "b": 2}
+print(dic["asa"])

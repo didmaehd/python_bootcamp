cc6180769720196204e6f3148bf2a19e83a27b70
didmaehd <didmaehd@gmail.com>
2023-02-17 18:59:56 +0900

    20230217#03

--- Day15#01_CoffeeMachine.py
+++ Day15#01_CoffeeMachine.py
@@ -59,7 +59,14 @@ while run_coffee :
         coin_list = ["quarters", "dimes","nickles","pennies"]
         for coin in coin_list:
             coins = int(input(f"How namy {coin} ? : "))
-            user_coin += coins
+            if coin == "quarters":
+                user_coin = coins * 25
+            if coin == "dimes":
+                user_coin = user_coin + coins * 10
+            if coin == "nickles":
+                user_coin = user_coin + coins * 5
+            if coin == "pennies":
+                user_coin = user_coin + coins * 2.5
         result =(check_coins(order,user_coin))
         
         if result == "fail_money":
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 
 
-def plus(num):
-    num = 100 + 100
-    return num
+user_coin = 0
+coin_list = ["quarters", "dimes","nickles","pennies"]
+for coin in coin_list:
+    coins = int(input(f"How namy {coin} ? : "))
+    if coin == "quarters":
+        user_coin = coins * 25
+    if coin == "dimes":
+        user_coin = user_coin + coins * 10
+    if coin == "nickles":
+        user_coin = user_coin + coins * 5
+    if coin == "pennies":
+        user_coin = user_coin + coins * 2.5
 
-i = 1
-print(plus(i))
-
-i = (plus(i))
-print(i)
+print (user_coin)

c565c2a6447021e92031c52bbeae7aa526918985
didmaehd <didmaehd@gmail.com>
2023-02-17 18:22:10 +0900

    20230217#03

--- Day15#01_CoffeeMachine.py
+++ Day15#01_CoffeeMachine.py
@@ -25,54 +25,53 @@ MENU = {
     }
 }
 resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
+    "water": 500,
+    "milk": 500,
+    "coffee": 500,
 }
+
+
+def check_coins(order, coin):
+    print(order,coin,MENU[order]["cost"])
+    if coin <  MENU[order]["cost"] :
+        return "fail_money"
+    else :
+        check_resources (order)
 
 def check_resources (order):
     if resources["water"] < MENU[order]["ingredients"]["water"] or resources["coffee"] < MENU[order]["ingredients"]["coffee"] or resources["milk"] < MENU[order]["ingredients"]["milk"] :
-        print ("Sorry! not enough resources")
+        return "fail_resources"
     else :
-        user_order(order)
+        return order
 
-def check_coins
-
-#make coffee
-def user_order(order):
-    if order :
-        #동전에 금액보다 적으면 실패 리턴
-        #리소스에서 커피 물 우유 양을 빼준다
-        resources["water"] -= MENU[order]["ingredients"]["water"] 
-        resources["coffee"] -= MENU[order]["ingredients"]["coffee"] 
-        resources["milk"] -= MENU[order]["ingredients"]["milk"] 
-        print(f"{order} here!")
-
+user_coin = 0
 run_coffee = True
 while run_coffee :
-#prompt user by asking 
+    user_coin == user_coin
     order = input("What would you like? (espresso/latte/cappuccino): ").lower()
-#turn off coffee machine 
     if order == "off":
         run_coffee = False
-#print report
     elif order == "report":
         for i in resources:
             print(f"{i} : {resources[i]}")
+        print(f"Money : {user_coin}")
     else :
-        check_resources (order)
-        # user_order(order)
-
-#Check resources sufficient
-#리소스가 하나라도 재료보다 적으면 실패 리턴
-
-#process coins
-#동전 인풋받아서 센트로 합산한 변수 만들기 = 
-
-def check_coins(order, user_coin):
-    if user_coin >  MENU[order]["cost"]:
-        pass
-#check transaction successul
-
+        coin_list = ["quarters", "dimes","nickles","pennies"]
+        for coin in coin_list:
+            coins = int(input(f"How namy {coin} ? : "))
+            user_coin += coins
+        result =(check_coins(order,user_coin))
         
-
+        if result == "fail_money":
+            print ("Not enough Money")
+            run_coffee = False
+        elif result == "fail_resources":
+            print ("Not enough resouces")
+            run_coffee = False
+        elif order :
+            resources["water"] -= MENU[order]["ingredients"]["water"] 
+            resources["coffee"] -= MENU[order]["ingredients"]["coffee"] 
+            resources["milk"] -= MENU[order]["ingredients"]["milk"] 
+            user_coin -= MENU[order]["cost"]
+            print(f"Here is ${user_coin} in change.")
+            print(f"{order} here!")
--- main.py
+++ main.py
@@ -1,27 +1,11 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
 
-print (MENU["espresso"]["ingredients"]["water"] - 10)
+
+def plus(num):
+    num = 100 + 100
+    return num
+
+i = 1
+print(plus(i))
+
+i = (plus(i))
+print(i)

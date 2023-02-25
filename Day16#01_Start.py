fb3727f6860a2d517ec5ec849f0c969a34b9eac8
didmaehd@gmail.com <didmaehd@gmail.com>
2023-02-25 21:45:45 +0900

    20230225#02

--- Day16#01_Start.py
+++ Day16#01_Start.py
@@ -16,9 +16,20 @@
 # print (my_screen.canvheight)
 # my_screen.exitonclick()
 
-import prettytable
-x = prettytable.PrettyTable()
-x.field_names = ["NO","NAME","POINT"]
-for i in range(10):
-    x.add_row([1, "tykim", 34])
-print(x)
+# import prettytable
+# x = prettytable.PrettyTable()
+# x.field_names = ["NO","NAME","POINT"]
+# for i in range(10):
+#     x.add_row([1, "tykim", 34])
+# print(x)
+
+ 
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pekemon Name",["Pikachu","Squirtle","Chanabder"])
+table.add_column("Type",["Electric","Water","Fire"])
+table.add_column("Level",[45,34,43])
+table.add_row(["Raichu","Electric",39])
+table.align = "l"
+table.align["Level"] = "r"
+print(table)
---
+++ Day16_Oop-Coffee-Machin-Project/main.py
@@ -0,0 +1,24 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+
+# 1 print report
+a = CoffeeMaker()
+print(a.report())
+b = MoneyMachine()
+print(b.report())
+
+# is_on = True
+# while is_on :
+#     choice = input("What would you like? (espresso/latte/cappuccino): ")
+#     if choice == "off":
+#         is_on = False
+
+
+
+# 2 check resources sufficient
+# 3 process Coins
+# 4 check transaction successful?
+# 5 Make Coffee
